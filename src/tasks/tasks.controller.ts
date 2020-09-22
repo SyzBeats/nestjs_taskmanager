8dff6975b0de1c951fca525485300d7c0120df53
Simeon Zimmermann <syzdevacc@gmail.com>
2020-09-22 18:35:11 +0200

    status update route in patch request

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,10 +3,18 @@
  * The Service is responsible for all the connected business logic
  */
 
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import Task, { TaskStatus } from './task.model';
-import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -17,11 +25,44 @@ export class TasksController {
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
+
+  /**
+   * @description Nest knows that the route is /:id
+   *              and expects the query Param to be a string
+   *              this will be forwarded to the service
+   */
+  @Get('/:id')
+  getTaskByID(@Param('id') id: string): Task {
+    return this.tasksService.getTaskByID(id);
+  }
+
+  /**
+   * @description create a task based on DTO
+   * @param createTaskDto
+   */
   @Post()
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  /**
+   * @description Update a task with an id and return updated Task
+   * @param id task that will be updated
+   */
+  @Patch('/:id/status')
+  updateTaskByID(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
   ): Task {
-    return this.tasksService.createTask(title, description);
+    return this.tasksService.updateTaskByID(id, status);
+  }
+
+  /**
+   * @description Delete a task with an id and return new Array
+   * @param id task that will be deleted
+   */
+  @Delete('/:id')
+  deleteTaskByID(@Param('id') id: string): Task[] {
+    return this.tasksService.deleteTaskByID(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,16 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import Task, { TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
+  /* GET ROUTES
+  --------------------*/
+
   getAllTasks(): Task[] {
     return this.tasks;
   }
 
-  createTask(title: string, description: string) {
+  getTaskByID(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  /* CREATE ROUTES
+  ------------------------*/
+  /**
+   * @description receive from controller, create a new task and return it
+   * @param createTaskDto
+   * @return Returns new created task
+   */
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
     let task: Task = {
       id: uuid(),
       title,
@@ -20,5 +37,26 @@ export class TasksService {
 
     this.tasks.push(task);
     return task;
+  }
+
+  /* PATCH ROUTES
+  --------------------------*/
+
+  updateTaskByID(id: string, status: TaskStatus): Task {
+    for (let task of this.tasks) {
+      if (task.id === id) {
+        task.status = status;
+        return task;
+      }
+    }
+  }
+
+  /* DELETE ROUTES
+  --------------------------*/
+
+  deleteTaskByID(id: string): Task[] {
+    let filtered = this.tasks.filter(task => task.id !== id);
+    this.tasks = filtered;
+    return filtered;
   }
 }
