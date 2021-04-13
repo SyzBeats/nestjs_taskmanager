cf7d85c54e40bc4da1b5aa23ca3cf267722074d5
Simeon Zimmermann <syzdevacc@gmail.com>
2021-04-13 23:21:55 +0200

    feature (backend): added validation pipe for auth payload

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(50)
+  // at least 1 uppercase, 1 lowercase, 1number or  special char
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Your provided password is not strong enough',
+  })
   password: string;
 }
