ac43e8eec75b6a5f88c5b6264ff2e8bb14602fad
Simeon Zimmermann <syzdevacc@gmail.com>
2021-04-18 00:21:36 +0200

    feature (backend): added config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    TasksModule,
-    TypeOrmModule.forRoot(typeOrmConfig),
-    AuthModule,
-    ConfigModule.forRoot(),
-  ],
+  imports: [TasksModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
 })
 export class AppModule {}
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,15 +1,26 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const {
+  type,
+  host,
+  username,
+  password,
+  database,
+  synchronize,
+  port,
+} = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'ec2-54-155-226-153.eu-west-1.compute.amazonaws.com',
-  port: 5432,
-  username: 'rqfzjugqvfkrqe',
-  password: '90b6331e8eea140ecb93d7bbfa3a34f0018416ee1dda78bf967bcc747b81d6cb',
-  database: 'd151d84rci3o5u',
+  type: type,
+  host: process.env.RDS_HOST || host,
+  port: process.env.RDS_PORT || port,
+  username: process.env.RDS_USERNAME || username,
+  password: process.env.RDS_PASSWORD || password,
+  database: process.env.RDS_DATABASE || database,
   // automatically takes care of all entities
   entities: [__dirname + '/../**/*.entity.{ts,js}'],
-  synchronize: true,
+  synchronize: synchronize,
   ssl: {
     rejectUnauthorized: false,
   },
