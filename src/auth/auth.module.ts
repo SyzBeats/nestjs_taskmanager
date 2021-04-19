44d0d5833db7bd09f0533715ec1f9ad79e57e977
Simeon Zimmermann <syzdevacc@gmail.com>
2021-04-19 22:17:16 +0200

    feature (backend): added auth packages jwt and started configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,27 @@
+import * as config from 'config';
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
+import { PassportModule } from '@nestjs/passport';
+
+const { secret, expiresIn } = config.get('jwt');
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || secret,
+      signOptions: {
+        expiresIn: process.env.JQT_EXPIRY || expiresIn,
+      },
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
